dedbfda1cef60c05531a431a0c02f7ef493ee98f
Jon Gilkison <jon.gilkison@gmail.com>
2024-12-02 01:52:33 +0700

    Update component to setup based. Remove CSS. Remove `fontControlled` and `filled` properties.

--- src/runtime/components/nuxt-icon.vue
+++ src/runtime/components/nuxt-icon.vue
@@ -1,50 +1,24 @@
+<script setup lang="ts">
+// file imported from https://github.com/gitFoxCode/nuxt-icons/blob/89e53649e5868c31fc97869918ede96504ae1a04/src/runtime/components/nuxt-icon.vue
+// with modifications
+defineProps<{
+  icon: object,
+  name: string,
+  width?: string,
+  height?: string,
+  fill?: string,
+  stroke?: string,
+}>();
+</script>
+
 <template>
   <component
     :is="icon"
-    :class="{
-      'nuxt-icon': fontControlled,
-      'nuxt-icon--fill': !filled,
+    :style="{
+      width: width || height ? (width ?? 'auto') : undefined,
+      height: width || height ? (height ?? 'auto') : undefined,
+      fill: fill && fill.length > 0 ? fill : undefined,
+      stroke: stroke && stroke.length > 0 ? stroke : undefined,
     }"
   />
 </template>
-
-<script lang="ts">
-  // file imported from https://github.com/gitFoxCode/nuxt-icons/blob/89e53649e5868c31fc97869918ede96504ae1a04/src/runtime/components/nuxt-icon.vue
-  // with modifications
-
-  export default {
-    props: {
-      filled: {
-        type: Boolean,
-        required: false,
-        default: false,
-      },
-      fontControlled: {
-        type: Boolean,
-        required: false,
-        default: true,
-      },
-      icon: {
-        type: Object,
-        required: true,
-      },
-      name: {
-        type: String,
-        required: false,
-      },
-    },
-  }
-</script>
-
-<style>
-  .nuxt-icon {
-    width: 1em;
-    height: 1em;
-    margin-bottom: 0.125em;
-    vertical-align: middle;
-  }
-  .nuxt-icon--fill,
-  .nuxt-icon--fill * {
-    fill: currentColor;
-  }
-</style>
